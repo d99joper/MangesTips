49d92d2d4b53c48cf89c6ba4164c2b0d598dbb91
d99joper <jonas@zooark.com>
2026-06-09 10:28:13 -0700

    topscorer autocomplete

--- Tips/Helpers/TopScorer.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="TopScorer.asmx.cs" Class="Tipset.Helpers.TopScorer" %>
---
+++ Tips/Helpers/TopScorersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tipset.Models; 
+
+namespace Tipset.Helpers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TopScorersController : ControllerBase
+    {
+        private readonly TopScorerRepository _topScorerRepository;
+
+        public TopScorersController(TopScorerRepository topScorerRepository)
+        {
+            _topScorerRepository = topScorerRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<string>> GetTopScorers([FromQuery] string? term)
+        {
+            try
+            {
+                // jQuery UI passes the typed text in the 'term' query variable
+                string? prefixText = term;
+
+                if (string.IsNullOrWhiteSpace(prefixText))
+                {
+                    return Ok(new List<string>());
+                }
+
+                IQueryable<Models.TopScorer> topScorers = _topScorerRepository.GetAllScorers();
+
+                // Split search words
+                string[] searchWords = prefixText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (searchWords.Length == 0)
+                {
+                    return Ok(new List<string>());
+                }
+
+                string strFirstName = searchWords[0];
+
+                if (searchWords.Length == 2)
+                {
+                    string strLastName = searchWords[1];
+                    // Simple StartsWith maps cleanly to SQL Server "LIKE 'value%'"
+                    topScorers = topScorers.Where(t =>
+                        t.FirstName.StartsWith(strFirstName) &&
+                        t.LastName.StartsWith(strLastName));
+                }
+                else
+                {
+                    // Simple StartsWith maps cleanly to SQL Server "LIKE 'value%'"
+                    topScorers = topScorers.Where(t =>
+                        t.FirstName.StartsWith(strFirstName) ||
+                        t.LastName.StartsWith(strFirstName));
+                }
+
+                List<string> listTopscorers = topScorers
+                    .Select(ts => ts.DisplayName)
+                    .ToList();
+
+                return Ok(listTopscorers);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+    }
+}
